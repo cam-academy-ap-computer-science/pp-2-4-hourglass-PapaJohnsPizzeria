d2fb716b2133c1565e0d4c253639c7574fdf9ea1
almajjoh000 <almajjoh000@CAM-LAB-10.bgsd.k12.wa.us>
2019-10-08 07:47:57 -0700

    this needs lots of work

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -1,7 +1,27 @@
 /*
-
+*Pseudocode
+*
+*TOP AND BOTTOM
+*one "plus"
+*link dashes to size
+*run forloop
+*one "plus"
+*
+*TOP HALF
+*
 */
 
 public class Hourglass {
-
+	public static final int SIZE=1;
+	public static void main(String[]args) {
+		public static void topAndBottom() {			
+			System.out.print("+");	
+			for(int c=1; c<=6; c++) {
+				System.out.print("-");
+			}
+			System.out.println("+");	
+			
+		}
+			
+	}
 }
