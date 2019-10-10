a1689964c24b44222db4b700b2d639f3f690c837
almajjoh000 <almajjoh000@CAM-LAB-10.bgsd.k12.wa.us>
2019-10-10 07:48:33 -0700

    getting slightly better

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -2,26 +2,43 @@
 *Pseudocode
 *
 *TOP AND BOTTOM
-*one "plus"
-*link dashes to size
+*one "|"
+*link quotes to size
 *run forloop
-*one "plus"
+*one "|"
 *
 *TOP HALF
+*forloop for the " \" then ":" then "/ "
+*
+*BOTTOM HALF
+*reverse top half
+*" /" then ":" then "\ "
 *
 */
 
 public class Hourglass {
-	public static final int SIZE=1;
-	public static void main(String[]args) {
-		public static void topAndBottom() {			
-			System.out.print("+");	
-			for(int c=1; c<=6; c++) {
-				System.out.print("-");
-			}
-			System.out.println("+");	
-			
+	public static final int SIZE=10;
+	public static void main(String[]args) {	
+		topAndBottom();
+		topHalf();
+	}
+	public static void topAndBottom() {			
+		System.out.print("|");	
+		for(int c=1; c<=SIZE; c++) {
+			System.out.print("\"");
 		}
-			
+		System.out.println("|");
+	}
+	public static void topHalf() {
+		for(int c=1; c<=4; c++) {
+			for(int a=1; a<=((SIZE/2)-1); a++) {
+			System.out.print(" ");
+			}
+			System.out.print("\\");
+			for(int n=1; n<=((SIZE/2)-1); n++) {
+				System.out.print(":");
+			}
+			System.out.println("/");
+		}
 	}
 }
