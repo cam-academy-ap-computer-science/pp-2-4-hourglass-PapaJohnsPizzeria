e15ab08bed2eabe674be4441eb2838d2d0295be0
almajjoh000 <almajjoh000@CAM-LAB-10.bgsd.k12.wa.us>
2019-10-15 07:49:32 -0700

    not done still confused

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -19,28 +19,26 @@
 public class Hourglass {
 	public static final int SIZE=10;
 	public static void main(String[]args) {	
-		topAndBottom();
+		topAndBottom();					//sequence shape
 		topHalf();
 	}
-	public static void topAndBottom() {			
+	public static void topAndBottom() {			//the caps
 		System.out.print("|");	
 		for(int c=1; c<=SIZE; c++) {
 			System.out.print("\"");
 		}
 		System.out.println("|");
 	}
-	public static void topHalf() {
+	public static void topHalf() {					//the \/ shape of hourglass
 		for(int c=1; c<=((SIZE/2)-1); c++) {
-			for(int a=1; a<= ; a++) {
 				for(int n=1; n<=1; n++) {
 					System.out.print(" ");
 				}
 			}
 			System.out.print("\\");
-			for(int t=1; t<=((SIZE/2)-1); t++) {
+			for(int t=1; t<=SIZE-2; t++) {
 				System.out.print(":");
 			}
 			System.out.println("/");
 		}
 	}
-}
