61134fbacd681630d74af14813d15a318d964e52
markcongdon <53883185+markcongdon@users.noreply.github.com>
2019-10-21 07:41:06 -0700

    Final Code submitted through email

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -8,7 +8,7 @@
 *one "|"
 *
 *TOP HALF
-*forloop for the " \" then ":" then "/ "
+*for loop for the " \" then ":" then "/ "
 *
 *BOTTOM HALF
 *reverse top half
@@ -17,28 +17,58 @@
 */
 
 public class Hourglass {
-	public static final int SIZE=10;
+	public static final int SIZE=4;
 	public static void main(String[]args) {	
-		topAndBottom();					//sequence shape
+		topAndBottom();
 		topHalf();
+		middle();
+		bottomHalf();
+		topAndBottom();
 	}
-	public static void topAndBottom() {			//the caps
+	public static void topAndBottom() {			
 		System.out.print("|");	
-		for(int c=1; c<=SIZE; c++) {
+		for(int c=1; c<=SIZE*2+2; c++) {
 			System.out.print("\"");
 		}
 		System.out.println("|");
 	}
-	public static void topHalf() {					//the \/ shape of hourglass
-		for(int c=1; c<=((SIZE/2)-1); c++) {
-				for(int n=1; n<=1; n++) {
-					System.out.print(" ");
-				}
+	
+//SPACES
+	public static void topHalf() {
+		for(int a=1; a<=SIZE; a++) {
+			for (int b=1; b<=a; b++) {
+				System.out.print(" ");
 			}
+		
+//CONE
 			System.out.print("\\");
-			for(int t=1; t<=SIZE-2; t++) {
+			for(int c=(SIZE*2+2)-(a*2); c>=1; c--) {
 				System.out.print(":");
 			}
 			System.out.println("/");
 		}
 	}
+	
+//middle bunch is aight
+	public static void middle() {
+		for(int d= 1; d<= SIZE+1; d++) {
+			System.out.print(" ");
+		}
+		System.out.println("||");
+	}
+	
+	
+//INVERSE CONE IS aight
+	public static void bottomHalf() {
+		for (int b = SIZE; b >= 1; b--) {
+			for (int space = 1; space <= b; space++) {
+				System.out.print(" ");
+			}
+			System.out.print("/");									
+			for (int dots = (2 * SIZE + 2) - (2 * b); dots >= 1; dots--) {
+				System.out.print(":");
+			}
+			System.out.println("\\");
+		}
+	}
+}
